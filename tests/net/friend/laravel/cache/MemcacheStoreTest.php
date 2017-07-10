d86afe32085d90c09fe558223a9a29d23af3dd4e
Nguyen Quang Huy <quanghuy.ico@gmail.com>
2017-07-10 20:58:46 +0700

    fix test case again

--- tests/net/friend/laravel/cache/MemcacheStoreTest.php
+++ tests/net/friend/laravel/cache/MemcacheStoreTest.php
@@ -2,27 +2,35 @@
 
 namespace Net\Friend\Laravel\Cache;
 
-use Memcached;
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class MemcacheStoreTest extends TestCase
 {
+    public function tearDown()
+    {
+        m::close();
+    }
+
     public function testGetReturnsNullWhenNotFound()
     {
-        $memcached = $this->getMock('Memcached', array('get'));
-        $memcached->expects($this->once())->method('get')->with($this->equalTo('foo:bar'))->will(
-            $this->returnValue(null)
-        );
-        $memcached->expects($this->once())->method('getResultCode')->will($this->returnValue(0));
+        $memcached = m::mock('stdClass');
+        $memcached->shouldReceive('get')->once()->with('foo:bar')->andReturn(null);
+        $memcached->shouldReceive('getResultCode')->once()->andReturn(0);
+//        $memcached = $this->getMock('Memcached', array('get'));
+//        $memcached->expects($this->once())->method('get')->with($this->equalTo('foo:bar'))->will(
+//            $this->returnValue(null)
+//        );
         $store = new \Illuminate\Cache\MemcachedStore($memcached, 'foo');
         $this->assertNull($store->get('bar'));
     }
 
     public function testMemcacheValueIsReturned()
     {
-        $memcached = $this->getMock('Memcached', array('get'));
-        $memcached->expects($this->once())->method('get')->will($this->returnValue('bar'));
-        $memcached->expects($this->once())->method('getResultCode')->will($this->returnValue(0));
+        $memcached = m::mock('stdClass');
+        $memcached->shouldReceive('get')->once()->andReturn('bar');
+        $memcached->shouldReceive('getResultCode')->once()->andReturn(0);
+//        $memcached->expects($this->once())->method('get')->will($this->returnValue('bar'));
         $store = new \Illuminate\Cache\MemcachedStore($memcached);
         $this->assertEquals('bar', $store->get('foo'));
     }
